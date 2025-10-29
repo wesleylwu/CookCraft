6c5ca1e93541c518d2811ec90144c354c440b9ba
Yubin Zhen <yubinzhen020105@gmail.com>
2025-10-28 23:19:12 -0700

    added 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,5 +1,17 @@
+import Image from "next/image";
+import maintenanceRobot from "@/public/404page/maintenanceRobot.webp";
+
 const NotFound = () => {
-  return <div>404 Page Not Found</div>;
+  return (
+    <div className="bg-cookcraft-white text-cookcraft-red flex h-screen flex-col items-center text-6xl font-bold">
+      <Image
+        src={maintenanceRobot}
+        alt="Maintenence Robot Image"
+        className="mt-28 mb-5 w-1/3"
+      />
+      404 Page Not Found
+    </div>
+  );
 };
 
 export default NotFound;
