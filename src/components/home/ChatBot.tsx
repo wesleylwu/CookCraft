ea4b64e07e2b2f88a642acaf603951a9b59e5fdf
xxiyun <wesleylwu@gmail.com>
2025-11-19 14:40:48 -0800

    add text animation to landing

--- src/components/home/ChatBot.tsx
+++ src/components/home/ChatBot.tsx
@@ -1,9 +1,33 @@
+"use client";
+import { useState, useEffect } from "react";
+import { motion } from "motion/react";
 import Image from "next/image";
 import logo from "@/public/home/cookCraftLogo.webp";
 const ChatBot = () => {
+  const fullText = "Welcome to CookCraft!";
+  const [displayedText, setDisplayedText] = useState<string>("");
+
+  useEffect(() => {
+    let index = 0;
+    const interval = setInterval(() => {
+      setDisplayedText(fullText.slice(0, index + 1));
+      index++;
+      if (index === fullText.length) {
+        clearInterval(interval);
+      }
+    }, 80);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div className="text-cookcraft-olive flex h-screen w-screen flex-col items-center justify-center text-6xl font-bold">
-      Welcome to CookCraft!
+      <motion.p
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 1 }}
+      >
+        {displayedText}
+      </motion.p>
       <Image src={logo} alt="Cook Craft Logo" className="mt-20 w-1/4" />
       <input
         type="text"
